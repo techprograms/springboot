0361fcb4f5ba008111abdd891f1b99487d91b7d4
techprograms <39184153+techprograms@users.noreply.github.com>
2022-09-22 18:53:19 +0530

    Update Course.java

--- src/main/java/com/srdt/springbootapp/Course.java
+++ src/main/java/com/srdt/springbootapp/Course.java
@@ -5,7 +5,7 @@ public class Course {
 	private long id;
 	private String name;
 	private String author;
-
+	//Constructor
 	public Course(long id, String name, String author) {
 		super();
 		this.id = id;
